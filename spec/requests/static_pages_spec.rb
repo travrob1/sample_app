714f1832938f30052405da2bbf0b69ed3048e254
Travis Robison <travrob1@gmail.com>
2014-06-13 17:44:51 -0600

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -51,7 +51,7 @@ describe "Static pages" do
     expect(page).to have_title(full_title('Contact'))
     click_link "Home"
     click_link "Sign up now!"
-    expect(page).to have_title(full_title('Sign Up'))
+    expect(page).to have_title(full_title('Sign up'))
   end
 
 end
